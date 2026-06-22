b108f1613d74d1bcb7dba9336fb5c9869c72b55c
Michael-AngelCevallos <michael.a.cevallos@gmail.com>
2026-06-22 12:36:50 -0500

    Addeding To cpp File Structure, Started Course Strucuture, Node Structure, and BinarySearchTree Class

---
+++ ProjectTwo.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//=========================================================
+// Course Structure
+//=========================================================
+
+struct Course {
+    string courseNumber;
+    string courseTitle;
+    vector<string> prerequisites;
+
+    Course() {
+
+    }
+
+    Course(string number, string title){
+        courseNumber = number;
+        courseTitle = title;
+    }
+
+};
+
+
+//===========================================================
+// Node Strucuture
+//===========================================================
+
+struct Node {
+    Course course;
+    Node* left;
+    Node* right;
+
+    Node() {
+        left = nullptr;
+        right = nullptr;
+    }
+
+    Node(Course aCourse) {
+        course = aCourse;
+        left = nullptr;
+        right = nullptr;
+    }
+
+};
+
+//=============================================================
+// Binary Search Tree Class
+//=============================================================
+
+class BinarySearchTree {
+    private:
+
+        Node* root;
+
+        void addNode(Node* node, Course course);
+        void inOrder(Node* node);
+
+    public:
+
+        BinarySearchTree();
+
+        void Insert(Course course);
+
+        void InOrder();
+
+        Course Search(string courseNumber);
+
+};
